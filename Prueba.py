d8e1f5ee82d7816b8cc3f172dc56a1e2aa866481
JDProyectos <juandavidrioscanon@gmail.com>
2025-07-01 12:47:59 -0500

    Definicion pagina a scrapear, rutas de transmi

--- Prueba.py
+++ Prueba.py
@@ -1,3 +1,19 @@
 import time
+import bs4
+from playwright.sync_api import sync_playwright
 
+
+
+#Proyecto dedicado al scraping web de la pagina transmilenio.gov.co
+#Se usara la libreria playwright para interactuar con la pagina web
+
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.transmilenio.gov.co/buscador_de_rutas")
+    print(page.title())
+    page.pause()
+    browser.close()
+    
 print("hello world")
