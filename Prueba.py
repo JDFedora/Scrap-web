fc2d362204643d3aa24387e8b43caff54bad24e7
JDProyectos <juandavidrioscanon@gmail.com>
2025-07-18 17:42:11 -0500

    rutas exportadas a CSV, mirar tema de errores y tiempos de carga de la pagina

--- Prueba.py
+++ Prueba.py
@@ -1,6 +1,7 @@
 import time
 import bs4
 import pandas as pd
+import numpy as np
 
 from playwright.sync_api import sync_playwright
 
@@ -40,11 +41,8 @@ with sync_playwright() as p:
             print("No se pudo hacer click en la pagina",i)
         page.expect_request_finished()
         page.wait_for_timeout(1200)
-
-
-
         
-
+  
     for a in Nombres_rutas:
         Indice_rutas.append(a.split()[0]) #añade el indice de la ruta (ej F14,1,B16...etc)
     for a in range(0,len(Nombres_rutas)):
@@ -52,6 +50,35 @@ with sync_playwright() as p:
     #escritura de los datos en un archivo csv
     df = pd.DataFrame({"Ruta":Indice_rutas,"Nombres":Nombres_rutas},index=None)
     df.to_csv('tabla.csv')
+    df = pd.DataFrame({"Href:":Href_rutas})
+    df.to_csv('Href rutas.csv')
+     # Extracción de las rutas y los nombres de cada una de las rutas
+    contador=0
+    for rutas in Href_rutas:
+        Estacion= []
+        page.goto(rutas)  # Navega a cada ruta
+        page.wait_for_timeout(2000)
+        page.expect_request_finished()
+        page.locator(".SearchResults > footer > div").first.scroll_into_view_if_needed()
+        page.wait_for_timeout(2000)
+        soup = bs4.BeautifulSoup(page.content(), "html.parser")
+        Estaciones = soup.find_all("div",class_ = "infoParada")
+        for a in Estaciones:
+            try:
+                Estacion.append(a.find("a").text.strip())  # Extrae el nombre de la estación
+            except Exception as e:
+                print("error de tipo: ",e)
+        #Estacion= np.unique(Estacion).tolist()  # Elimina duplicados
+        index=[]
+        for abc in Estacion:
+            index.append(Indice_rutas[contador])
+        contador+=1
+        df = pd.DataFrame({"Ruta":index,"Nombres":Estacion},index=None)
+        try:
+            df.to_csv(f'./Rutas/Rutas de:{index[0]}.csv')
+        except Exception as e:
+            df.to_csv(f'Rutas de:{contador}_error.csv')
+
     browser.close()
     
 print("Finalizado")
